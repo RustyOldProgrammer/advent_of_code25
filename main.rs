fc1ed69d73f33c7dacf484226e9230f1caf11db5
RustyOldProgrammer <theogrimmy+github@gmail.com>
2025-12-06 17:29:32 +0000

    1

--- main.rs
+++ main.rs
@@ -1,5 +1,17 @@
-
+struct rectangle{
+    width: u32,
+    height: u32,
+}
 
 fn main() {
+    let rect:(u32), (u32)=  rectangle{
+        width: 20
+        height:50
+    } 
+    println!("The area of the rectangle is {} square pixels", 
+    area(rect));
 
 }
+
+fn area(rectangle: &rectangle) -> u32:
+    rectangle.width * rectangle.height
